7471af5aaf8dc5d5847bdd491f794af488867276
Kurt <kurt@murrell.xyz>
2020-05-25 20:25:53 +1000

    Feature: add option to disable run messages (#5)

--- src/LiquidTestReports.Markdown/MarkdownTestLogger.cs
+++ src/LiquidTestReports.Markdown/MarkdownTestLogger.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 using LiquidTestReports.Core;
 using LiquidTestReports.Markdown.Resources;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel;
@@ -17,5 +19,21 @@ namespace LiquidTestReports.Markdown
 
         /// <inheritdoc/>
         protected override string GetTemplateContent() => Encoding.UTF8.GetString(Templates.MdReport);
+
+        /// <inheritdoc/>
+        protected override void OnInitialize(IReadOnlyDictionary<string, string> parameters)
+        {
+            var includeMessages = true;
+
+            if (parameters.TryGetValue(Constants.IncludeRunMessagesKey, out var includeRunMessages))
+            {
+                if (bool.TryParse(includeRunMessages, out var value))
+                {
+                    includeMessages = value;
+                }
+            }
+
+            LibraryParameters.Add(Constants.IncludeRunMessagesKey, includeMessages);
+        }
     }
 }
