83a76b11213de3ba47efc787e7014d4af490183b
Kurt <kurt@murrell.xyz>
2020-06-02 22:20:46 +1000

    Feature: add path split filter to use correct path character on non windows OS (#6)

---
+++ src/LiquidTestReports.Core/Filters/StringFilters.cs
@@ -0,0 +1,28 @@
+﻿using DotLiquid;
+using System.Runtime.InteropServices;
+
+namespace LiquidTestReports.Core.Filters
+{
+    /// <summary>
+    /// Filters for Strings.
+    /// </summary>
+    public static class StringFilters
+    {
+        /// <summary>
+        /// Split file paths based on OS path separator.
+        /// </summary>
+        /// <param name="input">string to split.</param>
+        /// <returns>Array of substrings.</returns>
+        public static string[] PathSplit(string input)
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                return StandardFilters.Split(input, @"\");
+            }
+            else
+            {
+                return StandardFilters.Split(input, @"/");
+            }
+        }
+    }
+}
