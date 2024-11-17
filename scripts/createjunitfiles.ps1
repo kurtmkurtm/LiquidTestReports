373845444b10f35322042340683cb12e660d4080
Kurt Murrell <5094408+kurtmkurtm@users.noreply.github.com>
2024-11-17 14:51:49 +1000

    feature: update dependencies and add .net 9 support (#44)
    * feature: update dependencies and add .net 9 support
    
    * tests: put integration tests in collection
    
    ---------
    
    Co-authored-by: Kurt <>

--- scripts/createjunitfiles.ps1
+++ scripts/createjunitfiles.ps1
@@ -1,9 +1,9 @@
 dotnet test ../test/SampleProject/SampleProject.Tests.xUnit -l "junit;LogFilePath=TestResults/xUnit-net8.0-junit-sample.xml" -f net8.0
 dotnet test ../test/SampleProject/SampleProject.Tests.NUnit -l "junit;LogFilePath=TestResults/NUnit-net8.0-junit-sample.xml" -f net8.0
 dotnet test ../test/SampleProject/SampleProject.Tests.MSTest -l "junit;LogFilePath=TestResults/MSTest-net8.0-junit-sample.xml" -f net8.0
-dotnet test ../test/SampleProject/SampleProject.Tests.xUnit -l "junit;LogFilePath=TestResults/xUnit-net6.0-junit-sample.xml" -f net6.0
-dotnet test ../test/SampleProject/SampleProject.Tests.NUnit -l "junit;LogFilePath=TestResults/NUnit-net6.0-junit-sample.xml" -f net6.0
-dotnet test ../test/SampleProject/SampleProject.Tests.MSTest -l "junit;LogFilePath=TestResults/MSTest-net6.0-junit-sample.xml" -f net6.0
+dotnet test ../test/SampleProject/SampleProject.Tests.xUnit -l "junit;LogFilePath=TestResults/xUnit-net9.0-junit-sample.xml" -f net9.0
+dotnet test ../test/SampleProject/SampleProject.Tests.NUnit -l "junit;LogFilePath=TestResults/NUnit-net9.0-junit-sample.xml" -f net9.0
+dotnet test ../test/SampleProject/SampleProject.Tests.MSTest -l "junit;LogFilePath=TestResults/MSTest-net9.0-junit-sample.xml" -f net9.0
 Move-Item -Path ../test/SampleProject/SampleProject.Tests.xUnit/TestResults/*-junit-sample.xml -Destination ../test/LiquidTestReports.Cli.Tests/JUnitTestInput/  -force
 Move-Item -Path ../test/SampleProject/SampleProject.Tests.MSTest/TestResults/*-junit-sample.xml -Destination ../test/LiquidTestReports.Cli.Tests/JUnitTestInput/  -force
 Move-Item -Path ../test/SampleProject/SampleProject.Tests.NUnit/TestResults/*-junit-sample.xml -Destination ../test/LiquidTestReports.Cli.Tests/JUnitTestInput/  -force
