95834890c2ba10c03871264820cc68b48f0a59ad
Kurt <>
2024-11-26 18:05:55 +1000

    fix: build warnings

--- src/LiquidTestReports.Core/Drops/TestResultSetDropCollection.cs
+++ src/LiquidTestReports.Core/Drops/TestResultSetDropCollection.cs
@@ -5,7 +5,7 @@ using System.Collections.ObjectModel;
 
 namespace LiquidTestReports.Core.Drops
 {
-    public class TestResultSetDropCollection : KeyedCollection<string, TestResultSetDrop>, IList<TestResultSetDrop>
+    public class TestResultSetDropCollection : KeyedCollection<string, TestResultSetDrop>
     {
         public TestResultSetDropCollection() : base() { }
         public TestResultSetDropCollection(IEnumerable<TestResultSetDrop> testResultSetDrops) : base()
@@ -15,14 +15,14 @@ namespace LiquidTestReports.Core.Drops
                 Add(testResultSetDrop);
             }
         }
-
+#if NETSTANDARD2_0
         public bool TryGetValue(string key, out TestResultSetDrop drop)
         {
             var hasKey = Contains(key);
             drop = hasKey ? this[key] : null;
             return hasKey;
         }
-
+# endif
         protected override string GetKeyForItem(TestResultSetDrop item) => item.Source;
 
     }
